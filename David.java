f865eac55488c1bc70f2cc6bae4cd038d3f8ca8b
Lance328 <ludavid@paterostechnologicalcollege.edu.ph>
2025-03-03 21:06:29 +0800

    Update David.java

--- David.java
+++ David.java
@@ -2,8 +2,8 @@ public class David{
     int age = 19;
 
     System.out.println("Hello! This is David contributing to the project.");
-    System.out.println("My current age is: " + age);
-    System.out.println("Next year, I will be: " + (age + 1));
+    System.out.println("My current age is:19 " + age);
+    System.out.println("Next year, I will be:20 " + (age + 1));
 
     }
 
